51dfb5c10248797e973c40b9e5558a6e626433ae
yamrita78 <yamrita78@gmail.com>
2025-01-29 15:43:07 +0530

    Update GMF.py to modify weight saving

--- GMF.py
+++ GMF.py
@@ -115,7 +115,12 @@ if __name__ == '__main__':
     topK = 10
     evaluation_threads = 1 #mp.cpu_count()
     print("GMF arguments: %s" %(args))
-    model_out_file = 'Pretrain/%s_GMF_%d_%d.weights.h5' %(args.dataset, num_factors, time())
+
+    # # When saving only the model weights, use:
+    # model_out_file = 'Pretrain/%s_GMF_%d_%d.weights.h5' %(args.dataset, num_factors, time())
+
+    # If you want to save the full model (architecture + weights)
+    model_out_file = 'Pretrain/%s_GMF_%d_%d.h5' %(args.dataset, num_factors, time())
     
     # Loading data
     t1 = time()
@@ -167,7 +172,11 @@ if __name__ == '__main__':
             if hr > best_hr:
                 best_hr, best_ndcg, best_iter = hr, ndcg, epoch
                 if args.out > 0:
-                    model.save_weights(model_out_file, overwrite=True)
+                    # # When saving only the model weights, use:
+                    # model.save_weights(model_out_file, overwrite=True)
+
+                    # If you want to save the full model (architecture + weights)
+                    model.save(model_out_file)
 
     print("End. Best Iteration %d:  HR = %.4f, NDCG = %.4f. " %(best_iter, best_hr, best_ndcg))
     if args.out > 0:
