6783e03399cd8dd00a84456c01cde79bfc68e2dc
yamrita78 <yamrita78@gmail.com>
2025-01-29 15:43:20 +0530

    Update MLP.py to modify weight saving

--- MLP.py
+++ MLP.py
@@ -123,7 +123,13 @@ if __name__ == '__main__':
     topK = 10
     evaluation_threads = 1 #mp.cpu_count()
     print("MLP arguments: %s " %(args))
-    model_out_file = 'Pretrain/%s_MLP_%s_%d.weights.h5' %(args.dataset, args.layers, time())
+    
+    # # When saving only the model weights, use:
+    #model_out_file = 'Pretrain/%s_MLP_%s_%d.weights.h5' %(args.dataset, args.layers, time())
+
+    
+    # If you want to save the full model (architecture + weights)
+    model_out_file = 'Pretrain/%s_MLP_%s_%d.h5' %(args.dataset, args.layers, time())
     
     # Loading data
     t1 = time()
@@ -172,7 +178,11 @@ if __name__ == '__main__':
             if hr > best_hr:
                 best_hr, best_ndcg, best_iter = hr, ndcg, epoch
                 if args.out > 0:
-                    model.save_weights(model_out_file, overwrite=True)
+                    # # When saving only the model weights, use:
+                    # model.save_weights(model_out_file, overwrite=True)
+
+                    # If you want to save the full model (architecture + weights)
+                    model.save(model_out_file)
 
     print("End. Best Iteration %d:  HR = %.4f, NDCG = %.4f. " %(best_iter, best_hr, best_ndcg))
     if args.out > 0:
